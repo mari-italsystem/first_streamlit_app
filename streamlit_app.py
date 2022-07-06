60d3fdc93c4c21bd48270ec6b1562194100b7b20
mari-italsystem <108808273+mari-italsystem@users.noreply.github.com>
2022-07-06 15:56:25 +0200

    Update streamlit_app.py
    Texte erfasst

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menue')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-boiled free-range Egg')
